98323fa8d740f9fd186d2f82dded5111fd38b406
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-03 08:59:00 +0530

    remove all files

--- src/programs/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "../../components"
-export { Study } from "./Study"
-export { Studies } from "./Studies"
--- src/studies/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "../../components"
-export { Lesson } from "./Lesson"
-export { Lessons } from "./Lessons"
