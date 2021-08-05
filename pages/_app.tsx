7b9e3f7bb86ccd118de82459aa4ede541dd06741
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-05 19:19:19 +0530

    add and initialize environment helper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import { EnvironmentHelper } from "@utils/index";
+
+EnvironmentHelper.init();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
