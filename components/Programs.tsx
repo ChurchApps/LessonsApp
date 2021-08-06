d0a30b3d8d19208dd0105422996fcc3f370bd657
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-06 10:37:40 +0530

    add provider name to programs section

--- components/Programs.tsx
+++ components/Programs.tsx
@@ -47,9 +47,20 @@ export function Programs({ programs, providers }: Props) {
   }
 
   const programsView = providers.map((provider) => {
-    return programs
+    const view = programs
       .filter((program) => program.providerId === provider.id)
       .map((p) => createProgram(p));
+
+    return (
+      view.length > 0 && (
+        <div key={provider.id}>
+          <h3 className="mb-4" style={{ fontWeight: "bold" }}>
+            {provider.name}
+          </h3>
+          {view}
+        </div>
+      )
+    );
   });
 
   return (
