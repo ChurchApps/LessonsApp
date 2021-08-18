7d9dac42caeac34829844a3a6124bce88c07d407
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-18 17:31:47 +0530

    add logout button

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,10 +12,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          {/* TODO - try and avoid this scripts by using react-bootstrap's components */}
-          <script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
-          <script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous" />
-          <script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous" />
         </body>
       </Html>
     );
