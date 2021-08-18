442c4ca7b86253142cc3da16f85833750f2e5640
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-18 09:58:32 +0530

    fix resource menu dropdown

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,9 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous" />
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous" />
         </body>
       </Html>
     );
