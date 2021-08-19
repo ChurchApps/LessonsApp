0267bd8bda1928311b281682cafab03d42b8899c
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-08-19 17:03:27 +0530

    remove lessons tab from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,25 +41,6 @@ export function Header() {
                 </a>
               </Link>
             </div>
-
-            <Col
-              className="d-none d-md-block"
-              style={{
-                borderLeft: "2px solid #EEE",
-                borderRight: "2px solid #EEE",
-                maxWidth: "703px",
-                margin: "0 15px",
-              }}
-            >
-              <ul
-                id="nav-main"
-                className="nav nav-fill d-flex overflow-hidden"
-                style={{ height: "55px" }}
-              >
-                <NavItems prefix="main" />
-              </ul>
-            </Col>
-
             <div className="d-flex align-items-center" id="navRight">
               {userAction}
             </div>
