68598b9e50b84b35d89338e726cc66ddc3c02f47
Jeremy Zongker <jeremy@trilitech.com>
2021-08-19 09:09:27 -0500

    Create CP link (#31)
    * fix auth redirection and add admin link to nav dropdown (#29)
    
    * fix redirection issues
    
    * remove lessons tab from header
    
    * remove redundant code
    
    * add admin link to header dropdown
    
    * Added control panel link
    
    Co-authored-by: Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router"
 import { Container, Dropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faUser, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "@/hooks/useAuth";
 import { UserHelper, Permissions } from "@/utils";
 
@@ -10,13 +10,18 @@ export function Header() {
   const { user, logout, loggedIn } = useAuth();
   const router = useRouter()
 
-  const adminItems = UserHelper.checkAccess(
-    Permissions.lessonsApi.lessons.edit
-  ) && (
+  const adminItems = UserHelper.checkAccess(Permissions.lessonsApi.lessons.edit) && (
     <Dropdown.Item as="button" onClick={() => router.push("/admin")}>
       <FontAwesomeIcon icon={faUser} /> Admin
     </Dropdown.Item>
   );
+
+  const cpItems = UserHelper.checkAccess(Permissions.lessonsApi.lessons.editSchedules) && (
+    <Dropdown.Item as="button" onClick={() => router.push("/cp")}>
+      <FontAwesomeIcon icon={faCalendarAlt} /> Schedules
+    </Dropdown.Item>
+  );
+
 
   const userAction = loggedIn ? (
     <Dropdown>
@@ -26,6 +31,7 @@ export function Header() {
 
       <Dropdown.Menu>
         {adminItems}
+        {cpItems}
         <Dropdown.Item as="button" onClick={logout}>
           <FontAwesomeIcon icon={faLock} /> Logout
         </Dropdown.Item>
