9d5af08aab8c439e44a514eb9f920b4d300feebb
Jeremy Zongker <jeremy@trilitech.com>
2021-08-26 16:45:34 -0500

    Allowed for duplicate slugs across programs/studies

--- src/pages/[programSlug]/[studySlug]/[lessonSlug]/index.tsx
+++ src/pages/[programSlug]/[studySlug]/[lessonSlug]/index.tsx
@@ -2,53 +2,20 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { Container, Row, Col } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import { Layout, Venues } from "@/components";
-import {
-  ApiHelper,
-  ProgramInterface,
-  StudyInterface,
-  LessonInterface,
-  ArrayHelper,
-  VenueInterface,
-  ResourceInterface,
-} from "@/utils";
+import { ApiHelper, ProgramInterface, StudyInterface, LessonInterface, ArrayHelper, VenueInterface, ResourceInterface } from "@/utils";
 
-type Props = {
-  program: ProgramInterface;
-  study: StudyInterface;
-  lesson: LessonInterface;
-  venues: VenueInterface[];
-  resources: ResourceInterface[];
-};
+type Props = { program: ProgramInterface; study: StudyInterface; lesson: LessonInterface; venues: VenueInterface[]; resources: ResourceInterface[]; };
 
-export default function LessonsPage({
-  program,
-  study,
-  lesson,
-  venues,
-  resources,
-}: Props) {
+export default function LessonsPage({ program, study, lesson, venues, resources, }: Props) {
+
   const video = lesson.videoEmbedUrl ? (
     <div className="videoWrapper">
-      <iframe
-        width="992"
-        height="558"
-        src={lesson?.videoEmbedUrl}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <iframe width="992" height="558" src={lesson?.videoEmbedUrl} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
     </div>
   ) : (
     <Row>
       <Col lg={{ span: 8, offset: 2 }}>
-        <img
-          src={lesson.image}
-          className="img-fluid profilePic"
-          alt={lesson.name}
-        />
-        <br />
-        <br />
+        <img src={lesson.image} className="img-fluid profilePic" alt={lesson.name} /><br /><br />
       </Col>
     </Row>
   );
@@ -123,39 +90,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const program: ProgramInterface = await ApiHelper.getAnonymous(
-    "/programs/public/slug/" + params.programSlug,
-    "LessonsApi"
-  );
-
-  const study: StudyInterface = await ApiHelper.getAnonymous(
-    "/studies/public/slug/" + params.studySlug,
-    "LessonsApi"
-  );
-
-  const lesson: LessonInterface = await ApiHelper.getAnonymous(
-    "/lessons/public/slug/" + params.lessonSlug,
-    "LessonsApi"
-  );
-
-  const venues: VenueInterface[] = await ApiHelper.getAnonymous(
-    "/venues/public/lesson/" + lesson.id,
-    "LessonsApi"
-  );
-
-  const resources: ResourceInterface[] = await ApiHelper.getAnonymous(
-    "/resources/public/lesson/" + lesson.id,
-    "LessonsApi"
-  );
+  const program: ProgramInterface = await ApiHelper.getAnonymous("/programs/public/slug/" + params.programSlug, "LessonsApi");
+  const study: StudyInterface = await ApiHelper.getAnonymous("/studies/public/slug/" + program.id + "/" + params.studySlug, "LessonsApi");
+  const lesson: LessonInterface = await ApiHelper.getAnonymous("/lessons/public/slug/" + study.id + "/" + params.lessonSlug, "LessonsApi");
+  const venues: VenueInterface[] = await ApiHelper.getAnonymous("/venues/public/lesson/" + lesson.id, "LessonsApi");
+  const resources: ResourceInterface[] = await ApiHelper.getAnonymous("/resources/public/lesson/" + lesson.id, "LessonsApi");
 
   return {
-    props: {
-      program,
-      study,
-      lesson,
-      venues,
-      resources,
-    },
+    props: { program, study, lesson, venues, resources, },
     revalidate: 30,
   };
+
 };
