b00176f94e6b8548f274672786db86af45a08450
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-08-28 10:47:28 +0530

    Add google analytics (#53)
    * update submodule
    
    * add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-activity/dist/Dots.css";
 import "@/styles/globals.css";
+import { useEffect } from "react"
 import type { AppProps } from "next/app";
-import { EnvironmentHelper } from "@/utils";
+import { useRouter } from "next/router"
+import { EnvironmentHelper, pageview } from "@/utils";
 import { AuthProvider } from "@/hooks/useAuth";
 
 EnvironmentHelper.init();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
