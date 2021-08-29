dfaf3094b0d5520826996a8ae4847899705598a1
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-08-29 12:06:43 +0530

    fix google analytics (#57)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export { ApiHelper, ArrayHelper, UserHelper } from "@/appBase/helpers";
 export { EnvironmentHelper } from "./EnvironmentHelper";
-export { Permissions } from "./Permissions"
+export { Permissions } from "./Permissions";
 
 export * from "./interfaces";
-export * from "./googleAnalyticsHelper"
+export { GoogleAnalyticsHelper } from "./GoogleAnalyticsHelper";
