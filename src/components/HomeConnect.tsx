d60d1a881011a33f0b68d35d9055212c1fcc3e5e
Jeremy Zongker <jeremy@trilitech.com>
2022-02-25 11:47:44 -0600

    Updated home page video link

--- src/components/HomeConnect.tsx
+++ src/components/HomeConnect.tsx
@@ -25,7 +25,7 @@ export function HomeConnect() {
                   <iframe
                     width="992"
                     height="558"
-                    src={"https://www.youtube.com/embed/VSEee0KOYCQ"}
+                    src={"https://www.youtube.com/embed/cOep9hdBey4"}
                     title="Lessons.church App"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
