e2b3c9ecf630621a7b52212399d71d5c6c36a85d
Jeremy Zongker <jeremy@trilitech.com>
2022-06-27 18:04:32 -0500

    Added a simple download map

---
+++ src/components/admin/Map.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { ApiHelper, CopySectionInterface, SectionInterface, VenueInterface } from "@/utils";
+import { InputBox, ErrorMessages, DisplayBox } from "../index";
+import { FormControl, getTooltipUtilityClass, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { ComposableMap, Geographies, Geography, ZoomableGroup, Marker } from "react-simple-maps"
+import { DateHelper } from "@/appBase/helpers";
+import ReactTooltip from "react-tooltip";
+
+type Props = {
+  programId: string;
+  startDate: Date;
+  endDate: Date;
+};
+
+export const Map: React.FC<Props> = (props) => {
+
+  const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-10m.json"
+  const [ipData, setIpData] = useState<any[]>([]);
+  const [tooltipIp, setTooltipIp] = useState<any>(null);
+
+  const loadData = () => {
+    const dateString = "?startDate=" + DateHelper.formatHtml5Date(props.startDate) + "&endDate=" + DateHelper.formatHtml5Date(props.endDate);
+    ApiHelper.get("/downloads/" + props.programId + "/geo" + dateString, "LessonsApi").then(d => setIpData(d));
+  }
+
+  const getMarkers = () => {
+    const result: JSX.Element[] = [];
+    ipData.forEach(ip => {
+      result.push(<Marker coordinates={[ip.lon, ip.lat]}><circle r={2} fill="#1976d2" onMouseOver={() => { setTooltipIp(ip) }} onMouseLeave={() => { setTooltipIp(null) }} /></Marker>);
+    })
+    //result.push(<Marker coordinates={[-95, 36]}><circle r={1} fill="#FF5533" /></Marker>);
+    return result;
+  }
+
+  const getTooltip = () => {
+    if (tooltipIp) {
+      return <>
+        {tooltipIp.city}, {tooltipIp.state}<br />
+        {tooltipIp.country}<br />
+        Total Downloads: {tooltipIp.totalDownloads}</>
+    } else return ""
+  }
+
+  useEffect(() => { if (props.programId) { loadData(); } }, [props.startDate, props.endDate, props.programId]);
+
+  if (tooltipIp) console.log(tooltipIp);
+  return (
+    <DisplayBox headerText="Map of Downloads" headerIcon="map">
+
+      <div>
+        <ComposableMap data-tip="">
+          <ZoomableGroup center={[0, 0]} zoom={1}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) => geographies.map(geo =>
+                <Geography key={geo.rsmKey} geography={geo} fill="#9998A3" stroke="#EAEAEC" />
+              )}
+            </Geographies>
+            {getMarkers()}
+          </ZoomableGroup>
+        </ComposableMap>
+        <ReactTooltip>{getTooltip()}</ReactTooltip>
+      </div>
+    </DisplayBox>
+  );
+}
