fe8d9f4a36e11c62f20bc483e18084a2dd268916
Jeremy Zongker <jeremy@trilitech.com>
2022-06-27 01:25:21 -0500

    Temp fix to set the stage correctly in production.

--- src/utils/EnvironmentHelper.ts
+++ src/utils/EnvironmentHelper.ts
@@ -9,6 +9,7 @@ export class EnvironmentHelper {
 
   static init = () => {
     let stage = process.env.STAGE;
+    stage = "prod"
 
     switch (stage) {
       case "staging": EnvironmentHelper.initStaging(); break;
