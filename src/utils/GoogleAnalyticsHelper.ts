6edcf142938c9326604b3e9e4571dd987d21e261
PhotoPaul <PhotoPaul@gmail.com>
2022-10-21 16:55:27 +0300

    Fixed window.gtag not defined on staging

--- src/utils/GoogleAnalyticsHelper.ts
+++ src/utils/GoogleAnalyticsHelper.ts
@@ -9,16 +9,18 @@ interface Event {
 
 export class GoogleAnalyticsHelper {
   static pageview(url: string) {
-    window.gtag("config", EnvironmentHelper.GoogleAnalyticsTag, {
-      page_path: url,
-    });
+    if(typeof window.gtag !== "undefined")
+      window.gtag("config", EnvironmentHelper.GoogleAnalyticsTag, {
+        page_path: url,
+      });
   }
 
   static gaEvent({ action, category, label, value }: Event) {
-    window.gtag("event", action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    });
+    if(typeof window.gtag !== "undefined")
+      window.gtag("event", action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+      });
   }
 }
