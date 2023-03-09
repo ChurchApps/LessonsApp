0cdd895b970eb130bb7b9cbf835f43c457a076f6
Jeremy Zongker <jeremy@zongker.net>
2023-03-09 12:06:03 -0600

    Add facade loader for vimeo

--- src/components/EmbeddedVideo.tsx
+++ src/components/EmbeddedVideo.tsx
@@ -1,5 +1,5 @@
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 type Props = {
   videoEmbedUrl: string;
@@ -7,10 +7,16 @@ type Props = {
 };
 
 export function EmbeddedVideo(props: Props) {
-
   const youTubeBase = "https://www.youtube.com/embed/";
-  let youTubeId = (props.videoEmbedUrl?.indexOf(youTubeBase)>-1) ? props.videoEmbedUrl?.replace(youTubeBase, "") : "";
+  const vimeoBase = "https://player.vimeo.com/video/";// 738981143?h=526752b554&badge=0&autopause=0&player_id=0&app_id=58479/embed
+  let youTubeId = (props.videoEmbedUrl?.indexOf(youTubeBase) > -1) ? props.videoEmbedUrl?.replace(youTubeBase, "") : "";
+  let vimeoId = (props.videoEmbedUrl?.indexOf(vimeoBase) > -1) ? props.videoEmbedUrl?.replace(vimeoBase, "").split("?")[0] : "";
+
   if (youTubeId) return <LiteYouTubeEmbed  id={youTubeId} title={props.title} poster="maxresdefault"  />;
+  else if (vimeoId) {
+    let html = "<lite-vimeo videoid=\"364402896\"></lite-vimeo>";
+    return (<span dangerouslySetInnerHTML={{ __html: html }} ></span>);
+  }
   else return (
     <div className="videoWrapper">
       <iframe
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
           <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" as="font" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <script async type="module" src="https://cdn.jsdelivr.net/npm/@slightlyoff/lite-vimeo@0.1.1/lite-vimeo.js"></script>
         </Head>
         <body>
           <Main />
