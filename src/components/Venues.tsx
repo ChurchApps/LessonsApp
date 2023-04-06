336fdb424041f3e25b5031fe4d1b9db788d0382f
Jeremy Zongker <jeremy@zongker.net>
2023-04-06 16:52:30 -0500

    Made tabs scrollable

--- src/components/Venues.tsx
+++ src/components/Venues.tsx
@@ -13,7 +13,7 @@ type Props = {
 };
 
 export function Venues(props: Props) {
-  const [selectedTab, setSelectedTab] = useState(props.venues[0].id);
+  const [selectedTab, setSelectedTab] = useState(props.venues[0]?.id || "");
 
   const venueViews = props.venues.map((v, idx) => {
     const resources: ResourceInterface[] = [];
@@ -48,7 +48,7 @@ export function Venues(props: Props) {
     const tabs:JSX.Element[] = [];
     props.venues.forEach((v) => { tabs.push(<Tab label={v.name} value={v.id} />) });
     return (<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <TabList onChange={(e, newValue) => {setSelectedTab(newValue);}}>
+      <TabList onChange={(e, newValue) => {setSelectedTab(newValue);}} variant="scrollable">
         {tabs}
       </TabList>
     </Box>);
