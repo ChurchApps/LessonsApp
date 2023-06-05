2b68f02d85e5f5a5cc65f79125b7e03cf1dad254
Jeremy Zongker <jeremy@zongker.net>
2023-06-05 10:56:18 -0500

    Fixed testimonial and admin images

--- src/components/admin/StudyEdit.tsx
+++ src/components/admin/StudyEdit.tsx
@@ -81,7 +81,7 @@ export function StudyEdit(props: Props) {
       <InputBox id="studyDetailsBox" headerText="Edit Study" headerIcon="layers" saveFunction={handleSave} cancelFunction={handleCancel} deleteFunction={handleDelete}>
         <ErrorMessages errors={errors} />
         <a href="about:blank" className="d-block" onClick={handleImageClick}>
-          <img src={study.image || "/images/blank.png"} className="profilePic d-block mx-auto" id="imgPreview" alt="study" />
+          <img src={study.image || "/images/blank.png"} className="profilePic d-block mx-auto img-fluid" id="imgPreview" alt="study" />
         </a>
         <br />
         <Grid container spacing={3}>
--- src/components/home/HomeTestimonials.tsx
+++ src/components/home/HomeTestimonials.tsx
@@ -2,20 +2,6 @@ import { Container, Grid } from "@mui/material";
 
 export function HomeTestimonials() {
 
-  /*
-  <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>Lessons.church provides fun, biblical lessons with materials designed for different age groups, which allows families to discuss the same lesson all together.</p>
-              <b>Sarah DeLitta</b><br />Bedford Outpost Church, VA
-            </Grid>
-            <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>We love how Lessons.Church has provided a multitude of resources at our fingertips. It makes scheduling super easy!</p>
-              <b>Chelsea Boldt</b><br />Venture Christian Church, TX
-            </Grid>
-            <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>Lessons.church is a valuable resource of engaging, biblical and practical curriculum that is easy to incorporate into our children’s ministry!</p>
-              <b>Erin B.</b><br />Lake City Christian Church, FL
-            </Grid>*/
-
   const getTestimonial = (name:string, church:string, text:string) => (
     <Grid item xs={12} md={6}>
       <div className="testimonial">
@@ -40,7 +26,7 @@ export function HomeTestimonials() {
           <h2>Testimonials</h2>
         </div>
         <Grid container spacing={3}>
-          {getTestimonial("Sarah DeLitta", "Bedford Outpost Church, VA", "Lessons.church provides fun, biblical lessons with materials designed for different age groups, which allows families to discuss the same lesson all together.")}
+          {getTestimonial("Sarah DeLitta", "One Church, NH", "Lessons.church provides fun, biblical lessons with materials designed for different age groups, which allows families to discuss the same lesson all together.")}
           {getTestimonial("Chelsea Boldt", "Venture Christian Church, TX", "We love how Lessons.Church has provided a multitude of resources at our fingertips. It makes scheduling super easy!")}
         </Grid>
       </Container>
