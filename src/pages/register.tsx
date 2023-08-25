47024b13df18022b80747be80c562e79c28bf8be
Himali Malvawala <himalimalvawala@gmail.com>
2023-08-25 15:10:37 +0530

    fix: lessons.church logo for /register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
   return (
     <Layout withoutNavbar withoutFooter>
       <div style={{ maxWidth: 350, marginLeft: "auto", marginRight: "auto" }}>
-        <img src="/images/logo.png" alt="Church Lessons" style={{ width: "100%", marginTop: 100, marginBottom: 60 }} />
+        <img src="/images/logo-login.png" alt="Church Lessons" style={{ width: "100%", marginTop: 100, marginBottom: 60 }} />
         <ErrorMessages errors={errors} />
 
         <div id="loginBox" style={{ backgroundColor: "#FFF", border: "1px solid #CCC", borderRadius: 5, padding: 20 }}>
