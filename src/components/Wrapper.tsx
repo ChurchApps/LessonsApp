f80ab402c28d0a0665cedbeb1f7a1dc9847c97ff
Jeremy Zongker <jeremy@zongker.net>
2023-10-11 21:07:33 -0500

    Added provider edit

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -17,6 +17,7 @@ export const Wrapper: React.FC<Props> = props => {
     if (typeof window !== "undefined") {
       const path = window.location.pathname;
       if (path.startsWith("/admin")) result = "admin";
+      else if (path.startsWith("/portal/thirdParty")) result = "external";
       else if (path.startsWith("/portal")) result = "cp";
     }
     return result;
@@ -27,6 +28,7 @@ export const Wrapper: React.FC<Props> = props => {
   tabs.push(<NavItem url="/" label="Home" icon="home" router={router} />);
   if (UserHelper.checkAccess(Permissions.lessonsApi.lessons.edit)) tabs.push(<NavItem url="/admin" label="Admin" icon="admin_panel_settings" router={router} selected={selectedTab === "admin"} key="admin" />);
   if (UserHelper.checkAccess(Permissions.lessonsApi.lessons.editSchedules)) tabs.push(<NavItem url="/portal" label="Schedules" icon="calendar_month" router={router} selected={selectedTab === "cp"} key="cp" />);
+  if (UserHelper.checkAccess(Permissions.lessonsApi.lessons.editSchedules)) tabs.push(<NavItem url="/portal/thirdParty" label="External Providers" icon="groups" router={router} selected={selectedTab === "external"} key="external" />);
 
   const navContent = <><List component="nav" sx={Themes.NavBarStyle}>{tabs}</List></>
 
---
+++ src/pages/portal/thirdParty.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { ApiHelper, DisplayBox, ExternalProviderInterface, SmallButton } from "@/utils";
+import { Wrapper } from "@/components/Wrapper";
+import { Grid, Icon } from "@mui/material";
+import { ProviderEdit } from "@/components/portal/ProviderEdit";
+
+export default function ThirdParty() {
+  const router = useRouter();
+  const { isAuthenticated } = ApiHelper;
+  const [providers, setProviders] = useState([]);
+  const [editProvider, setEditProvider] = useState<ExternalProviderInterface>(null);
+
+  const loadData = () => {
+    ApiHelper.get("/externalProviders", "LessonsApi").then((data: any) => {
+      setProviders(data);
+    });
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated) router.push("/login");
+    else loadData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getProviders = () => {
+    let result: JSX.Element[] = [];
+    return <table className="table">
+      <tbody>{getRows()}</tbody>
+    </table>;
+  }
+
+  const getRows = () => {
+    const result: JSX.Element[] = [];
+    providers.forEach((p) => {
+      result.push(
+        <tr className="scheduleRow" key={p.id}>
+          <td>
+            {p.name}
+          </td>
+          <td style={{textAlign:"right"}}>
+            <a href="about:blank" onClick={(e) => { e.preventDefault(); setEditProvider(p); }}><Icon>edit</Icon></a>
+          </td>
+        </tr>
+      );
+    });
+    return result;
+  };
+
+
+  const getEditContent = () => <SmallButton icon="add" onClick={() => { setEditProvider({}); }} />
+
+  return (
+    <Wrapper>
+      <h1>External Lesson Providers</h1>
+      <p>You can use lessons from other sources that support the <a href="https://support.churchapps.org/developer/open-lesson-schema.html" target="_blank">Open Lesson Format</a> in the Lessons.church and B1.church apps.  Manage those providers here.</p>
+      <Grid container spacing={3}>
+        <Grid item md={8} xs={12}>
+          <DisplayBox headerText="Providers" headerIcon="groups" editContent={getEditContent()}>
+            {getProviders()}
+          </DisplayBox>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          { editProvider && (<ProviderEdit provider={editProvider} updatedCallback={() => { setEditProvider(null); loadData(); }} />)}
+        </Grid>
+      </Grid>
+
+    </Wrapper>
+  );
+}
