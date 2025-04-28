0e545716c8a6d10d72a6bbfeb57fae386dd76325
Jeremy Zongker <jeremy@zongker.net>
2025-04-27 20:51:56 -0500

    Fixed gramatical errors

--- src/app/landing/ark/page.tsx
+++ src/app/landing/ark/page.tsx
@@ -22,8 +22,8 @@ export default async function ArkHome() {
       <div id="hero" className="arkHero">
         <Container fixed>
           <h1>Children's Ministry Can be Complicated</h1>
-          <h2 style={{color:"#24b8ff"}}>We Want to Make it Easy</h2>
-          <div style={{textAlign: "center"}}>
+          <h2 style={{ color: "#24b8ff" }}>We Want to Make it Easy</h2>
+          <div style={{ textAlign: "center" }}>
             <h3>High Quality Video Curriculum</h3>
             <h3>All-in-One Technology Solution</h3>
             <h3>Sunday Morning Support</h3>
@@ -33,20 +33,20 @@ export default async function ArkHome() {
       </div>
 
       <Videos />
-      <div className="homeSection" style={{backgroundColor:"#CCC"}}>
+      <div className="homeSection" style={{ backgroundColor: "#CCC" }}>
         <Container fixed>
           <Grid container spacing={4}>
 
-            <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>Lessons.church provides fun, biblical lessons with materials designed for different age groups, which allows families to discuss the same lesson all together.</p>
+            <Grid item md={4} xs={12} style={{ textAlign: "center" }}>
+              <p style={{ minHeight: 100, fontStyle: "italic" }}>Lessons.church provides fun, biblical lessons with materials designed for different age groups, allowing families to discuss the same lesson together.</p>
               <b>Sarah DeLitta</b><br />Bedford Outpost Church, VA
             </Grid>
-            <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>We love how Lessons.Church has provided a multitude of resources at our fingertips. It makes scheduling super easy!</p>
+            <Grid item md={4} xs={12} style={{ textAlign: "center" }}>
+              <p style={{ minHeight: 100, fontStyle: "italic" }}>We love how Lessons.Church has provided a multitude of resources at our fingertips. It makes scheduling super easy!</p>
               <b>Chelsea Boldt</b><br />Venture Christian Church, TX
             </Grid>
-            <Grid item md={4} xs={12} style={{textAlign:"center"}}>
-              <p style={{minHeight:100, fontStyle:"italic"}}>Lessons.church is a valuable resource of engaging, biblical and practical curriculum that is easy to incorporate into our children’s ministry!</p>
+            <Grid item md={4} xs={12} style={{ textAlign: "center" }}>
+              <p style={{ minHeight: 100, fontStyle: "italic" }}>Lessons.church is a valuable resource of engaging, biblical and practical curriculum that is easy to incorporate into our children's ministry!</p>
               <b>Erin B.</b><br />Lake City Christian Church, FL
             </Grid>
           </Grid>
@@ -59,10 +59,10 @@ export default async function ArkHome() {
             <Grid item md={1} sm={12}>
             </Grid>
             <Grid item md={4} sm={12}>
-              <img src="/images/landing/ark-videos.jpg" alt="Videos" className="img-fluid" style={{borderRadius:30}} />
+              <img src="/images/landing/ark-videos.jpg" alt="Videos" className="img-fluid" style={{ borderRadius: 30 }} />
             </Grid>
             <Grid item md={4} sm={12}>
-              <div className="arkUsp arkUspRight" style={{backgroundColor:"#DDD"}}>
+              <div className="arkUsp arkUspRight" style={{ backgroundColor: "#DDD" }}>
                 <h3>500+ High Quality Video Lessons</h3>
                 <p>Ark Kids is a complete video curriculum for children's ministry. Each lesson is designed to be engaging and easy to use. We programs for both elementary and preschool.  Each lesson is crafted with care to deliver powerful, impactful messages that keep kids engaged.</p>
               </div>
@@ -74,13 +74,13 @@ export default async function ArkHome() {
             <Grid item md={3} sm={12}>
             </Grid>
             <Grid item md={5} sm={12}>
-              <div style={{backgroundColor:"#DDD", position:"relative"}} className="arkUsp">
+              <div style={{ backgroundColor: "#DDD", position: "relative" }} className="arkUsp">
                 <h3>All-In-One Technology Solution</h3>
                 <p>Our technology platform is designed to make it easy for you to manage your curriculum. We provide a website for scheduling your lessons, an Android TV app that makes it simple to run your lessons, and a mobile app to guide your volunteers.</p>
               </div>
             </Grid>
             <Grid item md={4} sm={12}>
-              <img src="/images/landing/ark-tech.jpg" alt="Technology" className="img-fluid" style={{borderRadius:30}} />
+              <img src="/images/landing/ark-tech.jpg" alt="Technology" className="img-fluid" style={{ borderRadius: 30 }} />
             </Grid>
 
           </Grid>
@@ -90,10 +90,10 @@ export default async function ArkHome() {
             <Grid item md={1} sm={12}>
             </Grid>
             <Grid item md={4} sm={12}>
-              <img src="/images/landing/ark-support.jpg" alt="Support" className="img-fluid" style={{borderRadius:30}} />
+              <img src="/images/landing/ark-support.jpg" alt="Support" className="img-fluid" style={{ borderRadius: 30 }} />
             </Grid>
             <Grid item md={4} sm={12}>
-              <div className="arkUsp arkUspRight" style={{backgroundColor:"#DDD"}}>
+              <div className="arkUsp arkUspRight" style={{ backgroundColor: "#DDD" }}>
                 <h3>Sunday Morning Support</h3>
                 <p>We recognize the importance of your Sunday morning services and the need for prompt support. That's why we offer dedicated support on Sunday morning and throughout the week, ensuring that any questions or issues you may face are addressed quickly and efficiently.</p>
               </div>
