f6577f4c166ca76ac76b88c5c7c00d86893f76ee
Jeremy Zongker <jeremy@zongker.net>
2026-04-26 17:02:38 -0500

    Updated helpers/env settings

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const config = {
   reactStrictMode: true,
   env: {
     STAGE: process.env.STAGE,
-    NEXT_PUBLIC_ACCESS_API: process.env.NEXT_PUBLIC_ACCESS_API,
     NEXT_PUBLIC_LESSONS_API: process.env.NEXT_PUBLIC_LESSONS_API,
     NEXT_PUBLIC_CONTENT_ROOT: process.env.NEXT_PUBLIC_CONTENT_ROOT,
-    NEXT_PUBLIC_CHURCH_APPS_URL: process.env.NEXT_PUBLIC_CHURCH_APPS_URL,
     NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   },
   images: {
